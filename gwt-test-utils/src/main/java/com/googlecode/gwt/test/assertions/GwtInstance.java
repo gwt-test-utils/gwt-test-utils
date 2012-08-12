8fb5713323e78f5cfeac2d7d175861786dc5ec36
Gael Lazzari <gael.lazzari@gmail.com>
2012-08-12 09:13:34 +0200

    new assertion objects

---
+++ gwt-test-utils/src/main/java/com/googlecode/gwt/test/assertions/GwtInstance.java
@@ -0,0 +1,66 @@
+package com.googlecode.gwt.test.assertions;
+
+import com.googlecode.gwt.test.finder.GwtFinder;
+
+/**
+ * Wrapper arround a raw object retrieved via {@link GwtFinder#find(String...)}.
+ * 
+ * @author Gael Lazzari
+ * 
+ */
+public class GwtInstance {
+
+   static void validateNotNull(Class<?> type) {
+      if (type == null)
+         throw new IllegalArgumentException("Expected type cannot be null");
+   }
+
+   private final String[] identifier;
+   private final Object instance;
+
+   /**
+    * Creates a new <code>{@link GwtInstance}</code>.
+    * 
+    * @param identifier The targeted object's identifier.
+    */
+   protected GwtInstance(String... identifier) {
+      this.identifier = identifier;
+      this.instance = GwtFinder.find(identifier);
+   }
+
+   /**
+    * Cast the wrapped object to the specified class
+    * 
+    * @param type The type to convert to.
+    * @return The casted object
+    * 
+    * @throws NullPointerException if the wrapped object is null.
+    * @throws ClassCastException if the wrapped object cannot be cast to the supplied type.
+    * 
+    */
+   @SuppressWarnings("unchecked")
+   public <T> T ofType(Class<T> type) throws NullPointerException, ClassCastException {
+      validateNotNull(type);
+
+      if (instance == null) {
+         throw new NullPointerException("Object with identifier '" + identifierToString()
+                  + "' is null");
+      }
+
+      return (T) instance;
+   }
+
+   final Object getInstance() {
+      return instance;
+   }
+
+   private String identifierToString() {
+      StringBuilder sb = new StringBuilder();
+      for (String s : identifier) {
+         sb.append("'").append(s).append("', ");
+      }
+
+      return sb.substring(0, sb.length() - 2);
+   }
+
+}
---
+++ gwt-test-utils/src/main/java/com/googlecode/gwt/test/assertions/GwtWritableAssertionInfo.java
@@ -0,0 +1,51 @@
+package com.googlecode.gwt.test.assertions;
+
+import org.fest.assertions.core.WritableAssertionInfo;
+import org.fest.assertions.description.Description;
+import org.fest.assertions.description.TextDescription;
+
+/**
+ * Writable information about a GWT assertion.
+ * 
+ * @author Gael Lazzari
+ * 
+ */
+class GwtWritableAssertionInfo extends WritableAssertionInfo {
+
+   private String prefix;
+
+   @Override
+   public Description description() {
+      return (prefix == null) ? super.description()
+               : new TextDescription(computeDescribitionText());
+   }
+
+   @Override
+   public String descriptionText() {
+      return (prefix == null) ? super.descriptionText() : computeDescribitionText();
+   }
+
+   public String prefix() {
+      return prefix;
+   }
+
+   public void prefix(String prefix) {
+      this.prefix = prefix;
+   }
+
+   public Description superDescription() {
+      return super.description();
+   }
+
+   private String computeDescribitionText() {
+      StringBuilder sb = new StringBuilder();
+      sb.append(prefix.trim());
+
+      Description d = super.description();
+      if (d != null) {
+         sb.append(" ").append(d.value());
+      }
+      return sb.toString();
+   }
+
+}
