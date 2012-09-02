527b07e189822022082beb01c56e6f6063fa056e
Gael Lazzari <gael.lazzari@gmail.com>
2012-09-02 08:16:18 +0200

    Improve javadoc

--- gwt-test-utils/src/main/java/com/googlecode/gwt/test/BrowserSimulator.java
+++ gwt-test-utils/src/main/java/com/googlecode/gwt/test/BrowserSimulator.java
@@ -20,15 +20,21 @@ public interface BrowserSimulator {
 
    /**
     * Fire a Browser event loop end, so that commands scheduled with
-    * {@link Scheduler#scheduleDeferred(ScheduledCommand)},
-    * {@link Scheduler#scheduleFinally(ScheduledCommand)} and
-    * {@link Scheduler#scheduleFinally(RepeatingCommand)} can be processed. Eventual
+    * {@link Scheduler#scheduleFinally(ScheduledCommand)},
+    * {@link Scheduler#scheduleFinally(RepeatingCommand)} and
+    * {@link Scheduler#scheduleDeferred(ScheduledCommand)} can be processed. Eventual
     * {@link AsyncCallback} are also executed if there were some {@link RemoteService} call executed
     * during a test.
     * 
     * <p>
     * A browser event loop end is <strong>automatically triggered</strong> by gwt-test-utils before
     * and after a DOM event is processed.
+    * </p>
+    * 
+    * <p>
+    * {@link ScheduledCommand} and {@link RepeatingCommand} eventually scheduled by an
+    * {@link AsyncCallback} should also be triggered just after every queued async callbacks have
+    * been executed.
     * </p>
     */
    void fireLoopEnd();
