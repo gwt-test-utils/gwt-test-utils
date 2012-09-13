cf302d1cfee4d4f44c3e31ce655984d7187cfe95
Gael Lazzari <gael.lazzari@gmail.com>
2012-09-13 07:37:42 +0200

    Make ScrollPanelPatcher package protected

--- gwt-test-utils/src/main/java/com/googlecode/gwt/test/internal/patchers/ScrollPanelPatcher.java
+++ gwt-test-utils/src/main/java/com/googlecode/gwt/test/internal/patchers/ScrollPanelPatcher.java
@@ -1,20 +1,15 @@
 package com.googlecode.gwt.test.internal.patchers;
 
+import com.google.gwt.user.client.Element;
 import com.google.gwt.user.client.ui.ScrollPanel;
-import com.google.gwt.user.client.ui.UIObject;
 import com.googlecode.gwt.test.patchers.PatchClass;
 import com.googlecode.gwt.test.patchers.PatchMethod;
 
 @PatchClass(ScrollPanel.class)
-public class ScrollPanelPatcher {
-    
-    /**
-     * Simple patch to get the all test working that use ensureVisible
-     * @param scrollPanel
-     * @param item 
-     */
-    @PatchMethod
-    public static void ensureVisible(ScrollPanel scrollPanel, UIObject item) {
+class ScrollPanelPatcher {
 
-    }
+   @PatchMethod
+   static void ensureVisibleImpl(ScrollPanel scrollPanel, Element scroll, Element e) {
+
+   }
 }
