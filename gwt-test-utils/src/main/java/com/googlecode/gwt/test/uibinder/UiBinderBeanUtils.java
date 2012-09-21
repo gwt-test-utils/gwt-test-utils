f2f602927e3eaa3fd6ace3b6002d28245309ebf0
Kenichiro TANAKA <tanaka.kenichiro@gmail.com>
2012-09-21 23:11:26 +0900

    Add enum convert to UiBinderBeanUtils

--- gwt-test-utils/src/main/java/com/googlecode/gwt/test/uibinder/UiBinderBeanUtils.java
+++ gwt-test-utils/src/main/java/com/googlecode/gwt/test/uibinder/UiBinderBeanUtils.java
@@ -1,10 +1,14 @@
 package com.googlecode.gwt.test.uibinder;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.commons.beanutils.BeanUtilsBean;
 import org.apache.commons.beanutils.ConvertUtils;
+import org.apache.commons.beanutils.ConvertUtilsBean;
 import org.apache.commons.beanutils.Converter;
+import org.apache.commons.beanutils.PropertyUtils;
+import org.apache.commons.beanutils.PropertyUtilsBean;
 
 import com.google.gwt.user.client.ui.HasHorizontalAlignment.HorizontalAlignmentConstant;
 import com.google.gwt.user.client.ui.HasVerticalAlignment.VerticalAlignmentConstant;
@@ -23,72 +27,90 @@ import com.googlecode.gwt.test.exceptions.ReflectionException;
 @SuppressWarnings("deprecation")
 public class UiBinderBeanUtils {
 
-   private static final BeanUtilsBean UIBINDER_BEANUTILS = new BeanUtilsBean();
+  private static final BeanUtilsBean UIBINDER_BEANUTILS = new BeanUtilsBean(
+      new ConvertUtilsBean(){
+        @SuppressWarnings({"unchecked", "rawtypes"})
+        @Override
+        public Object convert(String value, Class clazz) {
+          if(clazz.isEnum()){
+            return Enum.valueOf(clazz, value);
+          }
+          else{
+            return super.convert(value, clazz);
+          }
+        }}, new PropertyUtilsBean());
 
-   static {
-      registerConverter(new Converter() {
+  static {
+    registerConverter(new Converter() {
 
-         @SuppressWarnings("rawtypes")
-         public Object convert(Class type, Object value) {
-            return UiBinderXmlUtils.parseHorizontalAlignment(value.toString());
-         }
-      }, HorizontalAlignmentConstant.class);
-
-      registerConverter(new Converter() {
-
-         @SuppressWarnings("rawtypes")
-         public Object convert(Class type, Object value) {
-            return UiBinderXmlUtils.parseVerticalAlignment(value.toString());
-         }
-      }, VerticalAlignmentConstant.class);
-
-      registerConverter(new Converter() {
-
-         @SuppressWarnings("rawtypes")
-         public Object convert(Class type, Object value) {
-            return UiBinderXmlUtils.parseTextAlignConstant(value.toString());
-         }
-      }, TextAlignConstant.class);
-   }
-
-   /**
-    * 
-    * @param o
-    * @param properties
-    * 
-    * @see BeanUtilsBean#populate(Object, Map)
-    */
-   public static void populateObject(Object o, Map<String, Object> properties) {
-      try {
-         UIBINDER_BEANUTILS.populate(o, properties);
-      } catch (Exception e) {
-         throw new ReflectionException("UiBinder error while setting properties for '"
-                  + o.getClass().getSimpleName() + "'", e);
+      @SuppressWarnings("rawtypes")
+      public Object convert(Class type, Object value) {
+        return UiBinderXmlUtils.parseHorizontalAlignment(value.toString());
       }
+    }, HorizontalAlignmentConstant.class);
 
-      // handle specifics
-      String[] styles = (String[]) properties.get("addStyleNames");
-      if (styles != null) {
-         for (String style : styles) {
-            if (o instanceof IsWidget) {
-               ((IsWidget) o).asWidget().addStyleName(style);
-            } else if (o instanceof UIObject) {
-               ((UIObject) o).addStyleName(style);
-            }
+    registerConverter(new Converter() {
 
-         }
+      @SuppressWarnings("rawtypes")
+      public Object convert(Class type, Object value) {
+        return UiBinderXmlUtils.parseVerticalAlignment(value.toString());
       }
-   }
+    }, VerticalAlignmentConstant.class);
 
-   /**
-    * 
-    * @param converter
-    * @param clazz
-    * 
-    * @see ConvertUtils#register(Converter, Class)
-    */
-   public static void registerConverter(Converter converter, Class<?> clazz) {
-      UIBINDER_BEANUTILS.getConvertUtils().register(converter, clazz);
-   }
+    registerConverter(new Converter() {
+
+      @SuppressWarnings("rawtypes")
+      public Object convert(Class type, Object value) {
+        return UiBinderXmlUtils.parseTextAlignConstant(value.toString());
+      }
+    }, TextAlignConstant.class);
+  }
+
+  /**
+   * 
+   * @param o
+   * @param properties
+   * 
+   * @see BeanUtilsBean#populate(Object, Map)
+   */
+  public static void populateObject(Object o, Map<String, Object> properties) {
+    try {
+      Map<String, Object> filteredProperties = new HashMap<String, Object>();
+      for (String key : properties.keySet()) {
+        if (PropertyUtils.isWriteable(o, key)) {
+          filteredProperties.put(key, properties.get(key));
+        }
+      }
+      UIBINDER_BEANUTILS.populate(o, filteredProperties);
+    } catch (Exception e) {
+      throw new ReflectionException(
+          "UiBinder error while setting properties for '"
+              + o.getClass().getSimpleName() + "'", e);
+    }
+
+    // handle specifics
+    String[] styles = (String[]) properties.get("addStyleNames");
+    if (styles != null) {
+      for (String style : styles) {
+        if (o instanceof IsWidget) {
+          ((IsWidget) o).asWidget().addStyleName(style);
+        } else if (o instanceof UIObject) {
+          ((UIObject) o).addStyleName(style);
+        }
+
+      }
+    }
+  }
+
+  /**
+   * 
+   * @param converter
+   * @param clazz
+   * 
+   * @see ConvertUtils#register(Converter, Class)
+   */
+  public static void registerConverter(Converter converter, Class<?> clazz) {
+    UIBINDER_BEANUTILS.getConvertUtils().register(converter, clazz);
+  }
 
 }
--- gwt-test-utils/src/test/java/com/googlecode/gwt/test/uibinder/UiBinderWithUiConstructorTest.java
+++ gwt-test-utils/src/test/java/com/googlecode/gwt/test/uibinder/UiBinderWithUiConstructorTest.java
@@ -5,18 +5,21 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 import com.googlecode.gwt.test.GwtTestTest;
+import com.googlecode.gwt.test.uibinder.UiConstructorWidget.AnotherType;
 import com.googlecode.gwt.test.uibinder.UiConstructorWidget.Type;
 
 public class UiBinderWithUiConstructorTest extends GwtTestTest {
 
-   @Test
-   public void instanciation() throws Exception {
-	  // Act
-	  UiBinderWithUiConstructor w = new UiBinderWithUiConstructor();
+  @Test
+  public void instanciation() throws Exception {
+    // Act
+    UiBinderWithUiConstructor w = new UiBinderWithUiConstructor();
 
-      // Assert
-      assertEquals(5, w.myWidget.size);
-      assertEquals(Type.T1, w.myWidget.type);
-   }
+    // Assert
+    assertEquals(5, w.myWidget.size);
+    assertEquals(Type.T1, w.myWidget.type);
+
+    assertEquals(AnotherType.T2, w.myWidget.anotherType);
+  }
 
 }
