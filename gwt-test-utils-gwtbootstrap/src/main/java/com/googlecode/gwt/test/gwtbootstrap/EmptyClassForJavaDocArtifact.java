50a330be01d852adaf85ee76f27d7677f20bd73a
Gael Lazzari <gael.lazzari@gmail.com>
2013-01-05 15:41:59 +0100

    add an empty to generate a javadoc artifact

---
+++ gwt-test-utils-gwtbootstrap/src/main/java/com/googlecode/gwt/test/gwtbootstrap/EmptyClassForJavaDocArtifact.java
@@ -0,0 +1,12 @@
+package com.googlecode.gwt.test.gwtbootstrap;
+
+/**
+ * An empty class to generate the gwt-test-utils-gwtbootstrap-javadoc.jar artifact which is
+ * mandatory to be pushed on Maven Central.
+ * 
+ * @author Gael Lazzari
+ * 
+ */
+public class EmptyClassForJavaDocArtifact {
+
+}
