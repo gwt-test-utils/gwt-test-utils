c3abd88817828c5cd1c43375342d4f145b95a433
elacazed <etienne.lacazedieu@gmail.com>
2014-02-26 09:48:09 +0100

    Fix code format

--- gwt-test-utils/src/main/java/com/googlecode/gwt/test/internal/utils/WebXmlUtils.java
+++ gwt-test-utils/src/main/java/com/googlecode/gwt/test/internal/utils/WebXmlUtils.java
@@ -30,162 +30,163 @@ import com.googlecode.gwt.test.exceptions.GwtTestException;
  */
 public class WebXmlUtils {
 
-    private static WebXmlUtils INSTANCE;
+   private static WebXmlUtils INSTANCE;
 
-    private static final String[] WAR_ROOTS = new String[]{"war/", "src/main/webapp/"};
+   private static final String[] WAR_ROOTS = new String[]{"war/", "src/main/webapp/"};
 
-    private static final String WEB_XML = "WEB-INF/web.xml";
+   private static final String WEB_XML = "WEB-INF/web.xml";
 
-    public static WebXmlUtils get() {
-        if (INSTANCE == null) {
-            INSTANCE = new WebXmlUtils();
-        }
-        return INSTANCE;
-    }
+   public static WebXmlUtils get() {
+      if (INSTANCE == null) {
+         INSTANCE = new WebXmlUtils();
+      }
+      return INSTANCE;
+   }
 
-    /**
-     * Gets a WebXmlUtils, or null if it fails.
-     * @return
-     */
-    public static WebXmlUtils maybeGet() {
-        try {
-            return get();
-        } catch (GwtTestConfigurationException exception) {
-            return null;
-        }
-    }
+   /**
+    * Gets a WebXmlUtils, or null if it fails.
+    * 
+    * @return
+    */
+   public static WebXmlUtils maybeGet() {
+      try {
+         return get();
+      } catch (GwtTestConfigurationException exception) {
+         return null;
+      }
+   }
 
-    private String firstWelcomeFile;
-    private final Set<String> listenerClasses;
-    // a map with servletUrl as key and serviceImpl className as value
-    private final Map<String, String> servletClassMap;
+   private String firstWelcomeFile;
+   private final Set<String> listenerClasses;
+   // a map with servletUrl as key and serviceImpl className as value
+   private final Map<String, String> servletClassMap;
 
-    private WebXmlUtils() {
-        InputStream is = getWebXmlAsStream();
-        try {
-            DocumentBuilder builder = XmlUtils.newDocumentBuilder();
-            Document document = builder.parse(is);
-            XPath xpath = XPathFactory.newInstance().newXPath();
-            servletClassMap = parseServletClassMap(document, xpath);
-            listenerClasses = parseListeners(document, xpath);
-            firstWelcomeFile = parseFirstWelcomFile(document, xpath);
-        } catch (Exception e) {
-            if (GwtTestException.class.isInstance(e)) {
-                throw (GwtTestException) e;
-            }
-            throw new GwtTestConfigurationException("Error while parsing web.xml", e);
-        } finally {
-            // close the stream
-            try {
-                is.close();
-            } catch (Exception ioException) {
-                // nothing to do
-            }
-        }
-    }
+   private WebXmlUtils() {
+      InputStream is = getWebXmlAsStream();
+      try {
+         DocumentBuilder builder = XmlUtils.newDocumentBuilder();
+         Document document = builder.parse(is);
+         XPath xpath = XPathFactory.newInstance().newXPath();
+         servletClassMap = parseServletClassMap(document, xpath);
+         listenerClasses = parseListeners(document, xpath);
+         firstWelcomeFile = parseFirstWelcomFile(document, xpath);
+      } catch (Exception e) {
+         if (GwtTestException.class.isInstance(e)) {
+            throw (GwtTestException) e;
+         }
+         throw new GwtTestConfigurationException("Error while parsing web.xml", e);
+      } finally {
+         // close the stream
+         try {
+            is.close();
+         } catch (Exception ioException) {
+            // nothing to do
+         }
+      }
+   }
 
-    public String getFirstWelcomeFile() {
-        return firstWelcomeFile;
-    }
+   public String getFirstWelcomeFile() {
+      return firstWelcomeFile;
+   }
 
-    public Set<String> getListenerClasses() {
-        return listenerClasses;
-    }
+   public Set<String> getListenerClasses() {
+      return listenerClasses;
+   }
 
-    public String getServletClass(String servletPath) {
-        return servletClassMap.get(servletPath);
-    }
+   public String getServletClass(String servletPath) {
+      return servletClassMap.get(servletPath);
+   }
 
-    private InputStream getWebXmlAsStream() {
-        for (String warRoot : WAR_ROOTS) {
-            try {
-                return new FileInputStream(warRoot + WEB_XML);
-            } catch (FileNotFoundException e) {
-                // try next
-            }
-        }
+   private InputStream getWebXmlAsStream() {
+      for (String warRoot : WAR_ROOTS) {
+         try {
+            return new FileInputStream(warRoot + WEB_XML);
+         } catch (FileNotFoundException e) {
+            // try next
+         }
+      }
 
-        throw new GwtTestConfigurationException("Cannot find 'web.xml' file ' for GWT module "
-                + GWT.getModuleName());
-    }
+      throw new GwtTestConfigurationException("Cannot find 'web.xml' file ' for GWT module "
+               + GWT.getModuleName());
+   }
 
-    private String parseFirstWelcomFile(Document document, XPath xpath)
+   private String parseFirstWelcomFile(Document document, XPath xpath)
             throws XPathExpressionException {
-        return (String) xpath.evaluate("/web-app/welcome-file-list/welcome-file", document,
-                XPathConstants.STRING);
-    }
+      return (String) xpath.evaluate("/web-app/welcome-file-list/welcome-file", document,
+               XPathConstants.STRING);
+   }
 
-    private Set<String> parseListeners(Document document, XPath xpath)
+   private Set<String> parseListeners(Document document, XPath xpath)
             throws XPathExpressionException {
-        Set<String> result = new HashSet<String>();
+      Set<String> result = new HashSet<String>();
 
-        NodeList listeners = (NodeList) xpath.evaluate("/web-app/listener", document,
-                XPathConstants.NODESET);
+      NodeList listeners = (NodeList) xpath.evaluate("/web-app/listener", document,
+               XPathConstants.NODESET);
 
-        for (int i = 0; i < listeners.getLength(); i++) {
-            Node listenerCandidate = listeners.item(i);
-            String listenerClassName = xpath.evaluate("listener-class", listenerCandidate);
-            result.add(listenerClassName);
-        }
+      for (int i = 0; i < listeners.getLength(); i++) {
+         Node listenerCandidate = listeners.item(i);
+         String listenerClassName = xpath.evaluate("listener-class", listenerCandidate);
+         result.add(listenerClassName);
+      }
 
-        return result;
-    }
+      return result;
+   }
 
-    private Map<String, String> parseMappingElements(Document document, XPath xpath)
+   private Map<String, String> parseMappingElements(Document document, XPath xpath)
             throws XPathExpressionException {
-        NodeList mappings = (NodeList) xpath.evaluate("/web-app/servlet-mapping", document,
-                XPathConstants.NODESET);
+      NodeList mappings = (NodeList) xpath.evaluate("/web-app/servlet-mapping", document,
+               XPathConstants.NODESET);
 
-        Map<String, String> mappingsMap = new HashMap<String, String>();
+      Map<String, String> mappingsMap = new HashMap<String, String>();
 
-        for (int i = 0; i < mappings.getLength(); i++) {
-            Node mapping = mappings.item(i);
-            String servletName = xpath.evaluate("servlet-name", mapping);
-            String urlPattern = xpath.evaluate("url-pattern", mapping);
-            if (!urlPattern.startsWith("/")) {
-                urlPattern = "/" + urlPattern;
-            }
-            mappingsMap.put(servletName, urlPattern);
-        }
-        return mappingsMap;
-    }
+      for (int i = 0; i < mappings.getLength(); i++) {
+         Node mapping = mappings.item(i);
+         String servletName = xpath.evaluate("servlet-name", mapping);
+         String urlPattern = xpath.evaluate("url-pattern", mapping);
+         if (!urlPattern.startsWith("/")) {
+            urlPattern = "/" + urlPattern;
+         }
+         mappingsMap.put(servletName, urlPattern);
+      }
+      return mappingsMap;
+   }
 
-    /**
-     * Retrieve Servlets information in the web.xml file.
-     * 
-     * @param document Object representing the web.xml file
-     * @param xpath
-     * @return A map with servletUrl as key and serviceImpl className as value
-     * @throws XPathExpressionException
-     */
-    private Map<String, String> parseServletClassMap(Document document, XPath xpath)
+   /**
+    * Retrieve Servlets information in the web.xml file.
+    * 
+    * @param document Object representing the web.xml file
+    * @param xpath
+    * @return A map with servletUrl as key and serviceImpl className as value
+    * @throws XPathExpressionException
+    */
+   private Map<String, String> parseServletClassMap(Document document, XPath xpath)
             throws XPathExpressionException {
 
-        Map<String, String> servletsMap = parseServletElements(document, xpath);
-        Map<String, String> mappingsMap = parseMappingElements(document, xpath);
+      Map<String, String> servletsMap = parseServletElements(document, xpath);
+      Map<String, String> mappingsMap = parseMappingElements(document, xpath);
 
-        // create the final map with servlets and servlet-mappings information
-        Map<String, String> result = new HashMap<String, String>();
-        for (Map.Entry<String, String> mappingsEntry : mappingsMap.entrySet()) {
-            result.put(mappingsEntry.getValue(), servletsMap.get(mappingsEntry.getKey()));
-        }
-        return result;
-    }
+      // create the final map with servlets and servlet-mappings information
+      Map<String, String> result = new HashMap<String, String>();
+      for (Map.Entry<String, String> mappingsEntry : mappingsMap.entrySet()) {
+         result.put(mappingsEntry.getValue(), servletsMap.get(mappingsEntry.getKey()));
+      }
+      return result;
+   }
 
-    private Map<String, String> parseServletElements(Document document, XPath xpath)
+   private Map<String, String> parseServletElements(Document document, XPath xpath)
             throws XPathExpressionException {
-        NodeList servlets = (NodeList) xpath.evaluate("/web-app/servlet", document,
-                XPathConstants.NODESET);
+      NodeList servlets = (NodeList) xpath.evaluate("/web-app/servlet", document,
+               XPathConstants.NODESET);
 
-        Map<String, String> servletsMap = new HashMap<String, String>();
+      Map<String, String> servletsMap = new HashMap<String, String>();
 
-        for (int i = 0; i < servlets.getLength(); i++) {
-            Node servlet = servlets.item(i);
-            String servletName = xpath.evaluate("servlet-name", servlet);
-            String servletClass = xpath.evaluate("servlet-class", servlet);
-            servletsMap.put(servletName, servletClass);
-        }
-        return servletsMap;
-    }
+      for (int i = 0; i < servlets.getLength(); i++) {
+         Node servlet = servlets.item(i);
+         String servletName = xpath.evaluate("servlet-name", servlet);
+         String servletClass = xpath.evaluate("servlet-class", servlet);
+         servletsMap.put(servletName, servletClass);
+      }
+      return servletsMap;
+   }
 
 }
