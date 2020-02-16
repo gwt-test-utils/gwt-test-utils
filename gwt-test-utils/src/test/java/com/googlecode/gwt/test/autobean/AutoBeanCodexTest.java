921ce735290eaf9fdd4487afee66c90bc48f40d6
Norbert Bartels <n.bartels@phpmonkeys.de>
2020-02-16 23:21:38 +0100

    NoIssue: fix travis ci build

--- gwt-test-utils/src/test/java/com/googlecode/gwt/test/autobean/AutoBeanCodexTest.java
+++ gwt-test-utils/src/test/java/com/googlecode/gwt/test/autobean/AutoBeanCodexTest.java
@@ -1,6 +1,7 @@
 package com.googlecode.gwt.test.autobean;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.ui.TreeItem;
 import com.google.web.bindery.autobean.shared.AutoBean;
 import com.google.web.bindery.autobean.shared.AutoBeanCodex;
 import com.google.web.bindery.autobean.shared.AutoBeanUtils;
@@ -18,6 +19,7 @@ public class AutoBeanCodexTest extends GwtTestTest {
 
     @Before
     public void before() {
+        new TreeItem();
         setIsClient(false);
     }
 
